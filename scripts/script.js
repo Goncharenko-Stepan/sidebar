452b0aeaf843e2fd05c63a36e4f5510b4664c8d0
Goncharenko-Stepan <bebrou1234@gmail.com>
2024-06-18 20:47:45 +0200

    Я добавил открытие и закрытие для sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 const firstDiv = document.querySelector(".firstDiv");
 const openButton = document.querySelector(".openButton");
+const sideBarDiv = document.querySelector(".sideBarDiv");
+
+openButton.addEventListener("click", () => {
+  sideBarDiv.classList.toggle("active");
+  if (sideBarDiv.classList.contains("active")) {
+    sideBarDiv.style.display = "block";
+  } else {
+    sideBarDiv.style.display = "none";
+  }
+});
